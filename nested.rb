96c40f1dba2cfaf5d7ab494d9fef98237a1add77
Reed Roffis <reedroffis18@gmail.com>
2020-12-01 20:23:16 +0000

    Automatically backed up by Learn

--- nested.rb
+++ nested.rb
@@ -1,4 +1,4 @@
-
+require "pry" 
 def hopper
 	programmer_hash = 
  		{
@@ -128,6 +128,8 @@ def adding_to_dennis
         :languages => ["C"]
       }
     }
-  programmer_hash[:dennis_ritchie][:languages] << "Assembly"
+  programmer_hash[:dennis_ritchie][:languages] << 
+  binding.pry 
+  "Assembly"
   programmer_hash
 end
